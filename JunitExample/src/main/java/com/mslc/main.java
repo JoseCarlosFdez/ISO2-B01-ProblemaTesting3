69324d32a4999e3b0342533786974f1ffcd12e86
Dmedina-coder <daniel.medina4@alu.uclm.es>
2024-11-29 16:15:25 +0100

    modularizado la comprobación de temperatura y humedad

--- JunitExample/src/main/java/com/mslc/main.java
+++ JunitExample/src/main/java/com/mslc/main.java
@@ -62,17 +62,17 @@ public class main {
 
     //Si la temperatura meteorológica está entre 0 y 15 grados, y no hay precipitaciones de agua, entonces es posible ir a hacer senderismo, si no se supera aforo del espacio previsto.
     public static boolean senderismo(){
-        return clima.getTemperatura() > 0 && clima.getHumedad() < 15 && !clima.isLlueve() && !montaña.aforoCompleto();
+        return temperaturaFresca() && !clima.isLlueve() && !montaña.aforoCompleto();
     }
 
     //Si la temperatura meteorológica está entre 15 y 25 grados, no llueve, y no está nublado y no hay una humedad relativa superior al 60%, entonces se puede ir a hacer turismo al aire libre, si la ciudad no tiene restricciones de confinamiento.
     public static boolean turismo(){
-        return clima.getTemperatura() > 15 && clima.getTemperatura() < 25 && clima.getHumedad() < 60 && !clima.isLlueve() && !clima.isNublado() && !ciudad.isRestricciones();
+        return temperaturaTemplada() && clima.getHumedad() < 60 && !clima.isLlueve() && !clima.isNublado() && !ciudad.isRestricciones();
     }
 
     //Si la temperatura meteorológica está entre 25 y 35 grados, y no llueve, la recomendación es irse de cañas, si el establecimiento no tiene problemas de aforo.
     public static boolean cañas(){
-        return clima.getTemperatura() > 25 && clima.getTemperatura() < 35 && !clima.isLlueve() && !bar.aforoCompleto();
+        return temperaturaCalida() && !clima.isLlueve() && !bar.aforoCompleto();
     }
 
     //Si la temperatura meteorológica es mayor que 30 grados, y no llueve, la recomendación es irse a la playa o a la piscina. La piscina no puede superar el aforo permitido.
@@ -82,6 +82,18 @@ public class main {
 
     public static boolean temperaturaFria(){
         return clima.getTemperatura() < 0 && clima.getHumedad() < 15;
+    }
+
+    public static boolean temperaturaFresca(){
+        return clima.getTemperatura() > 0 && clima.getHumedad() < 15;
+    }
+
+    public static boolean temperaturaTemplada(){
+        return clima.getTemperatura() > 15 && clima.getTemperatura() < 25;
+    }
+
+    public static boolean temperaturaCalida(){
+        return  clima.getTemperatura() > 25 && clima.getTemperatura() < 35;
     }
 
     public static void recomendacion(String recomen){
